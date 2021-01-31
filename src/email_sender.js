912aca4341046860aebc315db7718ad3f727b6b9
notapineapple <damnyorch@gmail.com>
2021-01-31 03:49:48 -0600

    feat: /email endpoint finished

--- src/email_sender.js
+++ src/email_sender.js
@@ -4,34 +4,51 @@ aws.config.update({ region: process.env.AWS_REGION || 'us-west-2' })
 
 const awsEmail = new aws.SES({apiVersion: '2010-12-01'})
 
-const emailBody = (name, lastName, email, company, phone, body, heardOf) => ({
+const emailBody = (name, lastName, email, company, phone, body) => ({
   Destination: {
     ToAddresses: [
-      email
+      'jorge@gendra.dev',
+      'erick.perez@gendra.mx'
     ]
   },
   Message: {
     Body: {
       Html: {
         Charset: "UTF-8",
-        Data: "HTML_FORMAT_BODY"
+        Data: `
+          <html lang="es">
+              <head>
+                  <meta charset="UTF-8">
+              <body>
+                  <p>Hay un nuevo contacto en la página de gendra.mx</p>
+                  <ul>
+                      <li>Nombre: ${name}</li>
+                      <li>Apellido: ${lastName}</li>
+                      <li>Correo: ${email}</li>
+                      <li>Compañia: ${company}</li>
+                      <li>Teléfono: ${phone}</li>
+                      <li>Mensaje: <p>${body}</p></li>
+                  </ul>
+              </body>
+          </html>
+        `
       },
       Text: {
         Charset: "UTF-8",
-        Data: "TEXT_FORMAT_BODY"
+        Data: `Hay un nuevo contacto en la página de gendra.mx\nNombre: ${name}\nApellido: ${lastName}\nCorreo: ${email}\nCompañia: ${company}\nTeléfono: ${phone}\nMensaje: ${body}`
       }
     },
     Subject: {
       Charset: 'UTF-8',
-      Data: 'Test email'
+      Data: 'Nuevo contacto - gendra.mx'
     }
   },
-  Source: 'no-reply@gendra.mx'
+  Source: 'no-reply@gendra.mx',
+  SourceArn: process.env.AWS_SES_SOURCE_ARN
 })
 
 export default async (req, res) => {
-  console.log(req.body)
-  const { name, lastName, email, company, phone, body, heardOf } = req.body
+  const { name, lastName, email, company, phone, body } = req.body
 
   await awsEmail.sendEmail(emailBody(
     name,
@@ -39,8 +56,7 @@ export default async (req, res) => {
     email,
     company,
     phone,
-    body,
-    heardOf
+    body
   )).promise();
 
   return res.send({ test: true })
